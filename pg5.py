8e102813de5a42bce4c2fe6ad01cf06e91499241
niranjansheelavant.ns@gmail.com <niranjansheelavant.ns@gmail.com>
2025-11-16 13:09:09 +0530

    save

---
+++ pg5.py
@@ -0,0 +1,10 @@
+# Program to calculate Simple Interest
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time (in years): "))
+
+# Calculating simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Displaying the result
+print("The Simple Interest is:", simple_interest)
